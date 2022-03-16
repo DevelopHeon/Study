7be8ac76f467c5d03e3267e0daea199a75840a35
devHeon <gmlgjs11@naver.com>
2022-03-16 14:12:01 +0900

    배열알고리즘

--- src/com/uni/baekjoon/chap05/Count.java
+++ src/com/uni/baekjoon/chap05/Count.java
@@ -3,7 +3,6 @@ package com.uni.baekjoon.chap05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Count {
 
--- src/com/uni/baekjoon/chap05/Remain.java
+++ src/com/uni/baekjoon/chap05/Remain.java
@@ -1,10 +1,21 @@
 package com.uni.baekjoon.chap05;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
 public class Remain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> hs = new HashSet<>();
+		
+		for(int i=0; i<10; i++) {
+			hs.add(Integer.parseInt(br.readLine())%42);
+		}
+		
+		System.out.println(hs.size());
 	}
 
 }
